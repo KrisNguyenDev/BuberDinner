8548693a6d655763ae8506878dd95547252e8452
ANPHATPLASTIC\cntt.ptda3 <minhhieu2032000@gmail.com>
2024-02-17 15:39:36 +0700

    initial commit

---
+++ BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        var authResult = _authenticationService.Register(request.FirstName, request.LastName, request.Email, request.PassWord);
+
+        var response = new AuthenticationResponse(authResult.Id, authResult.FirstName, authResult.LastName, authResult.Email, authResult.Token);
+
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        var authResult = _authenticationService.login(request.Email, request.PassWord);
+
+        var response = new AuthenticationResponse(authResult.Id, authResult.FirstName, authResult.LastName, authResult.Email, authResult.Token);
+
+        return Ok(response);
+    }
+}
