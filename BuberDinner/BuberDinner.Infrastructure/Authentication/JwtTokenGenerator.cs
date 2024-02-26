18f5a44aa30b9f5f3a8660e45fb20d158334d2dc
ANPHATPLASTIC\cntt.ptda3 <minhhieu2032000@gmail.com>
2024-02-26 17:12:16 +0700

    update jwtsettings

--- BuberDinner/BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ BuberDinner/BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -9,7 +10,12 @@ namespace BuberDinner.Infrastructure.Authentication;
 
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
+    private readonly IDateTimeProvider _dateTimeProvider;
 
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
     public string GenerateToken(Guid userId, string firstName, string lastName)
     {
         // Tạo một mảng byte để lưu trữ khóa
@@ -39,7 +45,7 @@ public class JwtTokenGenerator : IJwtTokenGenerator
 
         var sercurityToken = new JwtSecurityToken(
             issuer: "BuberDinner",
-            expires: DateTime.Now.AddDays(1),
+            expires: _dateTimeProvider.UtcNow.AddMinutes(60),
             claims: claims,
             signingCredentials: siginingCredentials);
 
