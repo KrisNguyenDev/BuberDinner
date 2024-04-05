50b98d355bb8352d4dc6195e716de73bf4a7b361
ANPHATPLASTIC\cntt.ptda3 <minhhieu2032000@gmail.com>
2024-04-05 17:19:29 +0700

    upload code

--- BuberDinner/BuberDinner.Application/Common/Errors/DuplicateEmailExeption.cs
+++ BuberDinner/BuberDinner.Application/Common/Errors/DuplicateEmailExeption.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BuberDinner.Application.Common.Errors;
 
-namespace BuberDinner.Application.Common.Errors
+public class DuplicateEmailExeption : Exception
 {
-    internal class DuplicateEmailExeption
-    {
-    }
 }
